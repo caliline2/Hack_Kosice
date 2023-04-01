a136a132a5db1c4b9adeda0790151678dfd9e1fb
pravoslavzilka <62677181+pravoslavzilka@users.noreply.github.com>
2023-04-02 00:51:42 +0200

    Update polygon.py

--- polygon.py
+++ polygon.py
@@ -10,7 +10,7 @@ total_logins = [first_polygon, second_polygon, third_polygon, fourth_polygon]
 final_geojson_string = '''{ "type": "FeatureCollection", "features": [  '''
 
 for polygon in total_logins:
-    final_geojson_string += '''{ "type": "Feature", "properties":{}, "geometry": { "type": "Polygon", "coordinates": [ [ [ '''+ str(polygon[0][0]) + ''', ''' + str(polygon[0][1]) + ''' ], [ ''' + str(polygon[3][0]) + ''', ''' + str(polygon[3][1]) + ''' ], [ ''' + str(polygon[2][0]) + ''', ''' + str(polygon[2][1]) + ''' ], [ ''' + str(polygon[1][0]) + ''', ''' + str(polygon[1][1]) + ''' ], [ ''' + str(polygon[0][0]) + ''', ''' + str(polygon[0][1]) + ''' ] ] ] } },'''
+    final_geojson_string += '''{ "type": "Feature", "properties":{}, "geometry": { "type": "Polygon", "coordinates": [ [ [ '''+ str(polygon[0][1]) + ''', ''' + str(polygon[0][0]) + ''' ], [ ''' + str(polygon[1][1]) + ''', ''' + str(polygon[1][0]) + ''' ], [ ''' + str(polygon[2][1]) + ''', ''' + str(polygon[2][0]) + ''' ], [ ''' + str(polygon[3][1]) + ''', ''' + str(polygon[3][0]) + ''' ], [ ''' + str(polygon[0][1]) + ''', ''' + str(polygon[0][0]) + ''' ] ] ] } },'''
 
 final_geojson_string = final_geojson_string[:-1] + "] }"
 print(final_geojson_string)
